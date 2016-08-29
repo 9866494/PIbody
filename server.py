52eeb98ee4b07328e5f0bf25fc8772f18b3f6918
awful <9866494@gmail.com>
2016-08-29 23:41:55 +0300

    Add port declaration

--- server.py
+++ server.py
@@ -69,7 +69,7 @@ try:
 
 
     if __name__ == '__main__':
-        socketio.run(app,host='0.0.0.0')
+        socketio.run(app,host='0.0.0.0', port = 8080)
 
 except (KeyboardInterrupt, SystemExit):
     GPIO.cleanup()
