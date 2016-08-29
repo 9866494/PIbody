25b6ba6b1b9dbd0102878882273f8e1385fcf41f
awful <9866494@gmail.com>
2016-08-29 23:04:08 +0300

    Made a class to control dc motor via l293d

--- devices/l293d.py
+++ devices/l293d.py
@@ -3,40 +3,58 @@
 from time import sleep
 
 import RPi.GPIO as GPIO
-import random
 
-CLOCK_PIN = 11
-DATA_PIN = 12
-LATCH_PIN = 13
+class L293d:
+    speed = 100
+
+    def __init__(self, forward_pin, backward_pin, speed_pin):
+        self.FORWARD_PIN = forward_pin
+        self.BACKWARD_PIN = backward_pin
+        self.SPEED_PIN = speed_pin
+
+        GPIO.setup(self.FORWARD_PIN, GPIO.OUT)
+        GPIO.setup(self.BACKWARD_PIN, GPIO.OUT)
+        GPIO.setup(self.SPEED_PIN, GPIO.OUT)
+
+        self.stop()
+
+        self.speed_pwm = GPIO.PWM(self.SPEED_PIN, self.speed)
+        self.speed_pwm.start(1)
+
+    def stop(self):
+        GPIO.output(self.BACKWARD_PIN, 0)
+        GPIO.output(self.FORWARD_PIN, 0)
+
+    def forward(self, speed = None):
+        self.setSpeed(speed)
+        GPIO.output(self.BACKWARD_PIN, 0)
+        GPIO.output(self.FORWARD_PIN, 1)
+
+    def backward(self, speed = None):
+        self.setSpeed(speed)
+        GPIO.output(self.BACKWARD_PIN, 1)
+        GPIO.output(self.FORWARD_PIN, 0)
+
+    def setSpeed(self, speed):
+        if (speed):
+            if speed > 100:
+                speed = 100
+            elif speed <= 10:
+                speed = 10
+
+            self.speed_pwm.ChangeDutyCycle(speed)
+            self.speed = speed
+        else:
+            self.speed_pwm.ChangeDutyCycle(self.speed)
+
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(CLOCK_PIN, GPIO.OUT)
-GPIO.setup(DATA_PIN, GPIO.OUT)
-GPIO.setup(LATCH_PIN, GPIO.OUT)
+l293d = L293d(11,12,13)
 
-GPIO.output(LATCH_PIN, 0)
-GPIO.output(CLOCK_PIN, 0)
-GPIO.output(DATA_PIN, 0)
-
-for k in range(8):
-    GPIO.output(LATCH_PIN, 0)
-    GPIO.output(DATA_PIN, 0)
-
-    print k
-
-    for i in range(8):
-        GPIO.output(CLOCK_PIN, 0)
-
-        if i == k:
-            GPIO.output(DATA_PIN, 1)
-        else:
-            GPIO.output(DATA_PIN, 0)
-
-        GPIO.output(CLOCK_PIN, 1)
-
-    GPIO.output(LATCH_PIN, 1)
-
-    sleep(1)
+l293d.forward(2000)
+sleep(2)
+l293d.backward(-1)
+sleep(2)
 
 GPIO.cleanup()
